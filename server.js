501a8e91a4864ffb33cfb54c6726420e03663fe1
Robert Fridlender <robertfridlender@gmail.com>
2023-01-03 19:04:51 -0500

    complete lab

---
+++ server.js
@@ -0,0 +1,30 @@
+// import modules
+
+import express from 'express'
+import { pokedex } from './data/pokedex.js';
+
+// Create Express app
+
+const app = express();
+
+// Configure the app (app.set)
+
+app.set('view engine', 'ejs');
+
+// Mount Middleware (app.use)
+
+app.use('/public', express.static('public'));
+
+// Mount routes
+
+app.get('/home', (req, res) => {
+  res.render('pokemon', {
+    pokedex: pokedex
+  });
+});
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+});
